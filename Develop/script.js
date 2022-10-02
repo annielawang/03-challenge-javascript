a3fee8d515ced649e47d8cb2b27b439cc6e7d275
Annie Li <annielawang@gmail.com>
2022-10-02 00:51:44 -0600

    added random number functions.

--- Develop/script.js
+++ Develop/script.js
@@ -18,18 +18,59 @@ function generatePassword() {
     var pswLengthNum = Number(pswLength);
     if(!isNaN(pswLengthNum) && pswLengthNum >= 8 && pswLengthNum <= 128) {
       var lowerCase = confirm("Do you want lowercase in your password?");
-        var upperCase = confirm("Do you want uppercase in your password?");
-          var numeric = confirm("Do you want number in your password?");
-            var speChar = confirm("Do you want special character in your password?");
+      var upperCase = confirm("Do you want uppercase in your password?");
+      var numeric = confirm("Do you want number in your password?");
+      var speChar = confirm("Do you want special character in your password?");
+      //TODO: if all false, alert.
+
+      return gen(pswLengthNum, lowerCase, upperCase, numeric, speChar);
     } else {
       alert("Please enter a valid password length.");
     }
-
-    var char = 
-    // return value to password
-    return randomPassword;//TODO:declare and add value to the variable
   }
   return;
+}
+
+function gen(length, lowerCaseFlag, upperCaseFlag, numericFlag, speCharFlag) {
+  var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var speChars = "!@#$%^&*()";
+
+  var chars = "";
+  var password = "";
+  if(lowerCaseFlag) {
+    chars += "abcdefghijklmnopqrstuvwxyz";
+    password += randomPicOne(lowerCaseChars);
+  } 
+
+  if(upperCaseFlag){
+    chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    password += randomPicOne(upperCaseChars);
+  }
+
+  if(numericFlag){
+    chars += "0123456789";
+    password += randomPicOne(numericChars);
+  }
+
+  if(speCharFlag){
+    chars += "!@#$%^&*()";
+    password += randomPicOne(speChars);
+  }
+
+  var leftCharsTotal = length - password.length;
+  
+  for (var i = 0; i < leftCharsTotal; i++) {
+    password += randomPicOne(chars);
+  }
+
+  return password;
+}
+
+function randomPicOne(charSet) {
+  var randomNumber = Math.floor(Math.random() * charSet.length);
+  return charSet.substring(randomNumber, randomNumber + 1);
 }
 
 // // Add event listener to generate button
